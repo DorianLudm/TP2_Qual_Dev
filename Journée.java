f9c6a992deedb0714042eb005a57350695f040fa
DorianLudm <dorianludm7@gmail.com>
2023-02-08 20:36:20 +0100

    Création de l'épruve du soir

--- Journée.java
+++ Journée.java
@@ -66,4 +66,26 @@ public class Journée {
         System.out.println("Auteur 1 :" + auteur1 + " Score : " + score1);
         System.out.println("Auteur 2 :" + auteur2 + " Score : " + score2);
     }
+
+    void creeEpreuveSoir(){
+        // Créer un style au hasard
+        Style style = Style.values()[alea(1,3)];
+        // Créer un nombre de spectateurs au hasard
+        int nb = alea(325,7535);
+        // Créer deux Auteurs au hasard
+        int choix1 = alea(0,2);
+        int choix2 = (choix1 + 1) % 3;
+        Auteur auteur1 = Auteur.lesAuteurs().get(choix1);
+        Auteur auteur2 = Auteur.lesAuteurs().get(choix2);
+        auteursSoir.add(auteur1);
+        auteursSoir.add(auteur2);
+        int score1 = nb * auteur1.getQualitéStyle(style);
+        int score2 = nb * auteur2.getQualitéStyle(style);
+        scoresSoir.add(score1);
+        scoresSoir.add(score2);
+        System.out.println("Epreuve du soir : ");
+        System.out.println("Style : " + style);
+        System.out.println("Auteur 1 :" + auteur1 + " Score : " + score1);
+        System.out.println("Auteur 2 :" + auteur2 + " Score : " + score2);
+    }
 }
